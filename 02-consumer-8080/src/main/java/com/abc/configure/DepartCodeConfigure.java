ad0c3d7f5115effbc646002f3aef610a7bd546f8
fangfangxu <472848022@qq.com>
2020-09-10 23:36:55 +0800

    Eureka Client 消费者
    Eureka Client 消费者

--- 02-consumer-8080/src/main/java/com/abc/configure/DepartCodeConfigure.java
+++ 02-consumer-8080/src/main/java/com/abc/configure/DepartCodeConfigure.java
@@ -1,11 +1,14 @@
 package com.abc.configure;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class DepartCodeConfigure {
+    //使用ribbon来进行负载均衡
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
