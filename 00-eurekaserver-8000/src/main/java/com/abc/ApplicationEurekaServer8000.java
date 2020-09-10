5c752b49c5d283b2207cb4204980769838a56690
fangfangxu <472848022@qq.com>
2020-09-10 22:30:00 +0800

    Eureka Server

--- 00-eurekaserver-8000/src/main/java/com/abc/ApplicationEurekaServer8000.java
+++ 00-eurekaserver-8000/src/main/java/com/abc/ApplicationEurekaServer8000.java
@@ -2,7 +2,10 @@ package com.abc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer //开启Eureka服务
 @SpringBootApplication
 public class ApplicationEurekaServer8000 {
 
