572c287d5c7ce6cbd945638f52c878c8f5e921d8
fangfangxu <472848022@qq.com>
2020-09-10 22:29:06 +0800

    commit

---
+++ 00-eurekaserver-8000/src/main/java/com/abc/ApplicationEurekaServer8000.java
@@ -0,0 +1,13 @@
+package com.abc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationEurekaServer8000 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationEurekaServer8000.class, args);
+    }
+
+}
