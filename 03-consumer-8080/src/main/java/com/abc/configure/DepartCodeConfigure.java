22f0c4eef9bd7712c6debef3f51ccd6df852e4e3
fangfangxu <472848022@qq.com>
2020-09-22 23:22:58 +0800

    openfeign
    openfeign

--- 03-consumer-8080/src/main/java/com/abc/configure/DepartCodeConfigure.java
+++ 03-consumer-8080/src/main/java/com/abc/configure/DepartCodeConfigure.java
@@ -3,9 +3,10 @@ package com.abc.configure;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
+@Controller
 public class DepartCodeConfigure {
     //使用ribbon来进行负载均衡
 //    @LoadBalanced
