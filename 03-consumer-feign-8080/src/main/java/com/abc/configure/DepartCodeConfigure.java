191822034edc77b57fce8c5fb07ee256297a9f99
fangfangxu <472848022@qq.com>
2020-10-02 19:32:59 +0800

    JavaConfig负载均衡>配置文件
    JavaConfig负载均衡>配置文件

--- 03-consumer-feign-8080/src/main/java/com/abc/configure/DepartCodeConfigure.java
+++ 03-consumer-feign-8080/src/main/java/com/abc/configure/DepartCodeConfigure.java
@@ -1,17 +1,18 @@
 package com.abc.configure;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class DepartCodeConfigure {
-    //使用ribbon来进行负载均衡
-//    @LoadBalanced
-//    @Bean
-//    public RestTemplate restTemplate(){
-//        return new RestTemplate();
-//    }
+     
+    //修改负载均衡策略为随机策略
+    @Bean
+    public IRule loadBanlanceRule(){
+        return new RoundRobinRule();
+    }
+
 }
