6dd9b44801f9521c201cf73684678a2714d00085
fangfangxu <472848022@qq.com>
2020-10-02 20:41:18 +0800

    自定义负载均衡策略
    自定义负载均衡策略

--- 03-consumer-feign-8080/src/main/java/com/abc/configure/DepartCodeConfigure.java
+++ 03-consumer-feign-8080/src/main/java/com/abc/configure/DepartCodeConfigure.java
@@ -1,18 +1,32 @@
 package com.abc.configure;
 
+import com.abc.balance.CustomRule;
 import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RandomRule;
-import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class DepartCodeConfigure {
-     
+
     //修改负载均衡策略为随机策略
+//    @Bean
+//    public IRule loadBanlanceRule(){
+//        return new RoundRobinRule();
+//    }
+
+    /**
+     * 修改负载均衡策略为：自定义策略
+     * @return
+     */
     @Bean
-    public IRule loadBanlanceRule(){
-        return new RoundRobinRule();
+    public IRule loadBanlanceRule() {
+        List<Integer> excludePorts = new ArrayList<>();
+        excludePorts.add(8083);
+        return new CustomRule(excludePorts);
     }
 
 }
